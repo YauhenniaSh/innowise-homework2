3b267f7a4fe35d8d760118baf9121d41975069a7
YauhenniaSh <yauhennia.shaptun@innowise.com>
2025-06-11 18:17:11 +0200

    enteties improved

--- src/main/java/com/example/usermanagement/model/Company.java
+++ src/main/java/com/example/usermanagement/model/Company.java
@@ -4,12 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = "id")
 @Entity
 @Builder
 @NoArgsConstructor
--- src/main/java/com/example/usermanagement/model/User.java
+++ src/main/java/com/example/usermanagement/model/User.java
@@ -1,12 +1,12 @@
 package com.example.usermanagement.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
+@ToString(exclude = {"address", "company"})
+@EqualsAndHashCode(of = "id")
 @Entity
 @Builder
 @NoArgsConstructor
